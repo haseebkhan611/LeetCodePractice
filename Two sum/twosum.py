96c28fd8fe816f3043f133a6e4aa974a2cd5c7d2
khanrhas <hkhan@thinkcei.com>
2024-06-25 21:19:43 -0900

    twosum with hashing

--- Two sum/twosum.py
+++ Two sum/twosum.py
@@ -3,6 +3,28 @@
 # algo:
 # if 
 
+def twoSumHash(arr1, k):
+    hashTable = {}
+    for i, nums in enumerate(arr1):
+        x = k - nums
+        if x in hashTable:
+            print(f"Found @ {hashTable[x]}, {x}")
+            print(f"{hashTable}")
+            return
+        hashTable[nums] = i
+    print('Not Found')
+    return
+
+def twoSum1(arr, k):
+    for i in range(len(arr)):
+        firstNum = arr[i]
+        for j in range(1,len(arr)):
+            secNum = arr[j]
+            if firstNum + secNum == k:
+                print(f"Target found. @ {i},  {j}")
+                return
+                
+
 def twoSum(arr1, k):
     arr = sorted(arr1)
     i = 0
@@ -17,6 +39,7 @@ def twoSum(arr1, k):
             return False
 
 if __name__ == '__main__':
-    arr = [3,2,4]
+    arr = [2,3,4]
     target = 6
-    twoSum(arr, target)
+    # twoSum(arr, target)
+    twoSumHash(arr, target)
